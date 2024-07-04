00a6718b55f4f6001c39e1995bfb701e091a6e1b
yana-mih <165790125+yana-mih@users.noreply.github.com>
2024-07-04 11:46:51 +0300

    Add files via upload

---
+++ EX_Mikheeva/ConsoleApp1/Student .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public string LastName { get; set; }
+
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+    }
+}
